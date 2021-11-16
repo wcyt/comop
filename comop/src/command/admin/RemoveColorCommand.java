ee30ea5890ff2f1bbdb778188b652441a29a8d04
koyama <koyama@H20304028>
2021-11-16 15:59:51 +0900

    RemoveColorCommand追加

---
+++ comop/src/command/admin/RemoveColorCommand.java
@@ -0,0 +1,13 @@
+package command.admin;
+
+import tera.AbstractCommand;
+import tera.ResponseContext;
+
+public class RemoveColorCommand extends AbstractCommand {
+
+	public ResponseContext execute(ResponseContext resc) {
+
+
+		return resc;
+	}
+}
