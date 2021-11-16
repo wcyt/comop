32f11cc0fe8013dbbe47624a397073b9c85ca465
Naka1313 <73629869+Naka1313@users.noreply.github.com>
2021-11-16 16:01:10 +0900

    Create GetProductListCommand.java

---
+++ comop/src/command/user/GetProductListCommand.java
@@ -0,0 +1,13 @@
+package command.user;
+
+import tera.AbstractCommand;
+import tera.ResponseContext;
+
+public class GetProductListCommand extends AbstractCommand {
+
+	public ResponseContext execute(ResponseContext resc) {
+
+
+		return resc;
+	}
+}
