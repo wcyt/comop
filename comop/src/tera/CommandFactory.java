aff1f03ba01dda37a1a67ad269792f9531da9dcc
koyama <koyama@H20304028>
2021-11-16 14:14:18 +0900

    a

--- comop/src/tera/CommandFactory.java
+++ comop/src/tera/CommandFactory.java
@@ -12,7 +12,7 @@ public abstract class CommandFactory {
 
 		try {
 			// プロパティファイルを読み込む
-			prop.load(new FileInputStream("C:\\pleiades-2019-12-java-win-64bit-jre_20200213\\pleiades\\workspace\\j2ee4/commands.properties"));
+			prop.load(new FileInputStream("c:/j2ee4.1/commands.properties"));
 
 			// Classクラスのインスタンスを利用して対応するクラスのインスタンス化を行う
 			command = (AbstractCommand) Class.forName(prop.getProperty(requestContext.getCommandPath())).newInstance();
