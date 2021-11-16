ded0debd4315ad73e86f0288e7b0d727f9cf968e
koyama <koyama@H20304028>
2021-11-16 15:57:18 +0900

    command.admin追加

--- comop/src/command/admin/AddColorCommand.java
+++ comop/src/command/admin/AddColorCommand.java
@@ -1,3 +1,13 @@
-public class AddProductCommand extends AbstractCommand {
+package command.admin;
 
+import tera.AbstractCommand;
+import tera.ResponseContext;
+
+public class AddColorCommand extends AbstractCommand {
+
+	public ResponseContext execute(ResponseContext resc) {
+
+
+		return resc;
+	}
 }
