558629624c8473a46c3ab59ded71abfd1d2363fd
wcyt <yw20304042@ga.tera-house.ac.jp>
2021-11-19 14:47:32 +0900

    UserBeanをコミット

--- comop/src/bean/UserBean.java
+++ comop/src/bean/UserBean.java
@@ -1,5 +1,124 @@
 package bean;
 
-public class UserBean {
+import java.io.Serializable;
 
+public class UserBean implements Serializable {
+	private int user_id;
+	private String name;
+	private String mail;
+	private String address;
+	private String first_name;
+	private String first_name_kana;
+	private String last_name;
+	private String last_name_kana;
+	private String tel;
+	private String postal_code;
+	private String password;
+	private int point;
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getFirst_name_kana() {
+		return first_name_kana;
+	}
+
+	public void setFirst_name_kana(String first_name_kana) {
+		this.first_name_kana = first_name_kana;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getLast_name_kana() {
+		return last_name_kana;
+	}
+
+	public void setLast_name_kana(String last_name_kana) {
+		this.last_name_kana = last_name_kana;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getPostal_code() {
+		return postal_code;
+	}
+
+	public void setPostal_code(String postal_code) {
+		this.postal_code = postal_code;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public boolean isUser_lapse() {
+		return user_lapse;
+	}
+
+	public void setUser_lapse(boolean user_lapse) {
+		this.user_lapse = user_lapse;
+	}
+
+	private boolean user_lapse;
 }
