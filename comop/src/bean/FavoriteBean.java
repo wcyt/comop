6b4b4261b2bf173cedae251a13f5cd7c804214ec
koyama <koyama@H20304028>
2021-11-22 16:30:48 +0900

    FavoriteBean修正

--- comop/src/bean/FavoriteBean.java
+++ comop/src/bean/FavoriteBean.java
@@ -5,8 +5,11 @@ import java.io.Serializable;
 public class FavoriteBean implements Serializable {
 	private int user_id;
 	private int product_id;
+	private String product_name;
+	private String product_image;
+	private int price;
 
-	public FavoliteBean() {}
+	public FavoriteBean() {}
 
 	public int getUser_id() {
 		return user_id;
@@ -22,5 +25,26 @@ public class FavoriteBean implements Serializable {
 
 	public void setProduct_id(int product_id) {
 		this.product_id = product_id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getProduct_image() {
+		return product_image;
+	}
+
+	public void setProduct_image(String product_image) {
+		this.product_image = product_image;
+	}
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
 	}
 }
