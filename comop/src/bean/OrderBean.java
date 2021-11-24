60b423d564ec86203c7441dd825fbd29d82e6f57
koyama <koyama@H20304028>
2021-11-24 15:01:39 +0900

    OrderBean修正

--- comop/src/bean/OrderBean.java
+++ comop/src/bean/OrderBean.java
@@ -9,6 +9,9 @@ public class OrderBean implements Serializable {
 	private int total_price;
 	private int product_id;
 	private int buy_count;
+	private String product_name;
+	private String product_image;
+	private int price;
 
 	public OrderBean() {}
 
@@ -51,5 +54,29 @@ public class OrderBean implements Serializable {
 
 	public void setBuy_count(int buy_count) {
 		this.buy_count = buy_count;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	public String getProduct_image() {
+		return product_image;
+	}
+
+	public void setProduct_image(String product_image) {
+		this.product_image = product_image;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
 	}
 }
