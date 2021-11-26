ed509f5fee07842f584fa554eef04ec93df0300b
iori445 <is20304021@ga.tera-house.ac.jp>
2021-11-26 15:44:01 +0900

    MySQLDaoFactory更新

--- comop/src/daofactory/MySQLDaoFactory.java
+++ comop/src/daofactory/MySQLDaoFactory.java
@@ -1,11 +1,86 @@
 package daofactory;
 
 import dao.AbstractDaoFactory;
+import dao.admin.ColorManagementDAO;
+import dao.admin.MySQLColorManagementDAO;
+import dao.admin.MySQLPointProductManagementDAO;
+import dao.admin.MySQLStockDAO;
+import dao.admin.MySQLUserManagementDAO;
+import dao.admin.PointProductManagementDAO;
+import dao.admin.ProductManagementDAO;
+import dao.admin.StockDAO;
+import dao.admin.UserManagementDAO;
+import dao.cart.CartDAO;
+import dao.cart.MySQLCartDAO;
+import dao.favorite.FavoriteDAO;
+import dao.favorite.MySQLFavoriteDAO;
+import dao.order.MySQLOrderDAO;
+import dao.order.MySQLPointOrderDAO;
+import dao.order.OrderDAO;
+import dao.order.PointOrderDAO;
+import dao.product.MySQLProductDAO;
+import dao.product.ProductDAO;
+import dao.user.MySQLUserDAO;
+import dao.user.UserDAO;
 
 public class MySQLDaoFactory extends AbstractDaoFactory {
-	/*
-	public ProductsDao getProductsDao() {
-		return new MySQLProductsDao();
+
+	@Override
+	public ColorManagementDAO getColorManagementDAO() {
+		return  new MySQLColorManagementDAO();
 	}
-	*/
+	@Override
+	public UserManagementDAO getUserManagementDAO() {
+		return new MySQLUserManagementDAO();
+	}
+	@Override
+	public ProductManagementDAO getProductManagementDAO() {
+		return MySQLProductManagementDAO();
+	}
+
+	@Override
+	public PointProductManagementDAO getPointProductManagementDAO() {
+		return new MySQLPointProductManagementDAO();
+	}
+
+	@Override
+	public StockDAO getStockDAO() {
+		return new MySQLStockDAO();
+	}
+
+	@Override
+	public FavoriteDAO getFavoriteDAO() {
+		return new MySQLFavoriteDAO();
+	}
+
+	@Override
+	public CartDAO getCartDAO() {
+		return new MySQLCartDAO();
+	}
+
+	@Override
+	public ProductDAO getProductDAO() {
+		return new MySQLProductDAO();
+	}
+
+	@Override
+	public PointProductManagementDAO getPointProductDAO() {
+		return new MySQLPointProductManagementDAO();
+	}
+
+	@Override
+	public UserDAO getUserDAOo() {
+		return new MySQLUserDAO();
+	}
+
+	@Override
+	public OrderDAO getOrderDAO() {
+		return new MySQLOrderDAO();
+	}
+
+	@Override
+	public PointOrderDAO getPointOrderDAO() {
+		return new MySQLPointOrderDAO();
+	}
+
 }
