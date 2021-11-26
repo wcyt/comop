4c17414ea409dcdd0a3705676f7dce254988e114
koyama <koyama@H20304028>
2021-11-26 16:13:49 +0900

    MySQLProductDAO中身追加

--- comop/src/dao/product/ProductDAO.java
+++ comop/src/dao/product/ProductDAO.java
@@ -10,5 +10,6 @@ public interface ProductDAO {
 	 public List<ProductBean> getProductsList(Map<String,String[]> parameters);
 	 public List<ProductBean> getProductDetail(String product_id);
 	 public List<ProductBean> searchProducts(String key);
+	 public int getStock_quantity(String product_id);
 
 }
