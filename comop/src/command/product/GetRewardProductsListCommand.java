1afe9b9977ce9bd2917fd7a1d6aa694323d45eb5
Takeru <73629816+ti880@users.noreply.github.com>
2021-11-29 10:14:21 +0900

    ポイント商品

--- comop/src/command/product/GetRewardProductsListCommand.java
+++ comop/src/command/product/GetRewardProductsListCommand.java
@@ -1,12 +1,22 @@
 package command.product;
 
+import java.util.List;
+
 import command.AbstractCommand;
+import dao.product.PointProductDAO;
+import daofactory.AbstractDaoFactory;
 import tera.ResponseContext;
 
 class GetRewardProductsListCommand extends AbstractCommand {
 
 	public ResponseContext execute(ResponseContext resc) {
+		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
+		PointProductDAO ppdao = factory.getPointProductDAO();
 
+		List pointlist = ppdao.getRewardProductsList();
+
+		resc.setResult(pointlist);
+		resc.setTarget("/WEB-INF/jsp/rewardProductsList.jsp");
 
 		return resc;
 	}
