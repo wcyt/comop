39a905ffcf4f8ade8d45e1f70a4f973e776c2822
wcyt <yw20304042@ga.tera-house.ac.jp>
2021-11-29 11:27:19 +0900

    loginをコミット

--- comop/src/command/user/LoginCommand.java
+++ comop/src/command/user/LoginCommand.java
@@ -2,7 +2,6 @@ package command.user;
 
 import bean.UserBean;
 import command.AbstractCommand;
-import dao.user.MySQLUserDAO;
 import dao.user.UserDAO;
 import daofactory.AbstractDaoFactory;
 import tera.RequestContext;
@@ -14,42 +13,19 @@ public class LoginCommand extends AbstractCommand {
 	public ResponseContext execute(ResponseContext resc) {
 		RequestContext reqc = getRequestContext();
 
-
-		String[] mails = reqc.getParameter("mail");
-		String[] passwords = reqc.getParameter("password");
-
 		String mail = reqc.getParameter("mailAddress")[0];
 		String password = reqc.getParameter("password")[0];
-
-		String mail = mails[0];
-		String password = passwords[0];
 
 		UserBean ub = new UserBean();
 		ub.setMail(mail);
 		ub.setPassword(password);
 
-		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
-		MySQLUserDAO msud = factory.getMySQLUserDAO();
-
-<<<<<<< HEAD
 		System.out.println("beanまでは北");
 
-=======
-		resc.setResult(msud.login(ub));
-
->>>>>>> branch 'main' of git@github.com:wcyt/comop.git
 		AbstractDaoFactory daoFactory = AbstractDaoFactory.getFactory();
 		UserDAO userDAO = daoFactory.getUserDAO();
 		userDAO.login(mail, password);
 
-<<<<<<< HEAD
-=======
-		// adminの場合
-		if (mail == "admin" && password == "admin") {
-			resc.setTarget("admin");
-		}
-
->>>>>>> branch 'main' of git@github.com:wcyt/comop.git
 		resc.setTarget("top");
 
 		return resc;
