e50e951f258fc4272839d04507ce0054dd6f13eb
koyama <koyama@H20304039>
2021-11-29 11:14:52 +0900

    logoutできたきがする

--- comop/src/command/user/LoginCommand.java
+++ comop/src/command/user/LoginCommand.java
@@ -3,29 +3,24 @@ package command.user;
 import bean.UserBean;
 import command.AbstractCommand;
 import dao.user.MySQLUserDAO;
+import dao.user.UserDAO;
 import daofactory.AbstractDaoFactory;
 import tera.RequestContext;
 import tera.ResponseContext;
 
-<<<<<<< HEAD
-public class LoginCommand  extends AbstractCommand {
-	boolean isRegist=false;
-=======
 public class LoginCommand extends AbstractCommand {
+	boolean isRegist=false;
 
->>>>>>> branch 'main' of git@github.com:wcyt/comop.git
 	public ResponseContext execute(ResponseContext resc) {
 		RequestContext reqc = getRequestContext();
 
-<<<<<<< HEAD
 		String[] mails = reqc.getParameter("mail");
 		String[] passwords = reqc.getParameter("password");
-=======
+
 		String mail = reqc.getParameter("mailAddress")[0];
 		String password = reqc.getParameter("password")[0];
->>>>>>> branch 'main' of git@github.com:wcyt/comop.git
 
-<<<<<<< HEAD
+
 		String mail = mails[0];
 		String password = passwords[0];
 
@@ -36,7 +31,6 @@ public class LoginCommand extends AbstractCommand {
 
 		resc.setResult(msud.login(ub));
 
-=======
 		AbstractDaoFactory daoFactory = AbstractDaoFactory.getFactory();
 		UserDAO userDAO = daoFactory.getUserDAO();
 		userDAO.login(mail, password);
@@ -47,7 +41,6 @@ public class LoginCommand extends AbstractCommand {
 		}
 
 		resc.setTarget("top");
->>>>>>> branch 'main' of git@github.com:wcyt/comop.git
 
 		return resc;
 	}
