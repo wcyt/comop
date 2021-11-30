61000cb036695842decdcffbe4cbf2c7add92459
iori445 <is20304021@ga.tera-house.ac.jp>
2021-11-30 12:24:58 +0900

    Connector.java変更（ConnectionManagerのメソッド追加）

--- comop/src/dao/Connector.java
+++ comop/src/dao/Connector.java
@@ -5,23 +5,62 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connector {
+	private static Connector connector = null;
 	private static Connection cn = null;
-	public static Connection connect() {
+	private Connector() {}
+
+	//Singleton pattern
+	public static Connector getInstance() {
+		if(connector == null) {
+			connector = new Connector();
+		}
+		return connector;
+	}
+
+	public  Connection connect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/comopdb?characterEncoding=UTF-8&serverTimezone=JST","comop","comop");
-			cn.setAutoCommit(false);
-		}catch(Exception e){
+		} catch(Exception e){
 			e.printStackTrace();
 		}
 		return cn;
 	}
-	public static void close() {
-		try {
-			cn.close();
 
-		}catch(SQLException e) {
+	public void close() {
+		try {
+			if(cn != null) {
+				cn.close();
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void beginTransaction() {
+		if(cn == null) {
+			connect();
+		}
+		try {
+			cn.setAutoCommit(false);
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void commit() {
+		try {
+			cn.commit();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void rollback() {
+		try {
+			cn.rollback();
+		} catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
