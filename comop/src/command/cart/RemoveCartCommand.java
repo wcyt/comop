081147e6b20d81e3da70d486950f10c37bbb1b12
koyama <koyama@H20304028>
2022-01-14 10:45:43 +0900

    RemoveCartCommand修正

--- comop/src/command/cart/RemoveCartCommand.java
+++ comop/src/command/cart/RemoveCartCommand.java
@@ -1,6 +1,10 @@
 package command.cart;
 
+import java.util.List;
+
+import bean.CartBean;
 import command.AbstractCommand;
+import dao.Connector;
 import dao.cart.CartDAO;
 import daofactory.AbstractDaoFactory;
 import tera.RequestContext;
@@ -16,10 +20,25 @@ public class RemoveCartCommand  extends AbstractCommand {
 		String user_id = reqc.getParameter("user_id")[0];
 		String product_id = reqc.getParameter("product_id")[0];
 
+		//トランザクションを開始する
+		Connector.getInstance().beginTransaction();
+
 		//カートの中身を削除
 		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
 		CartDAO cartDAO = factory.getCartDAO();
 		cartDAO.removeCart(user_id,product_id);
+
+		//カートの中身を取得
+		int userid=Integer.parseInt(user_id);
+		CartDAO cd = factory.getCartDAO();
+		List<CartBean> carts = cd.getCartList(userid);
+		resc.setResult(carts);
+
+		//トランザクションを終了する
+		Connector.getInstance().commit();
+
+				//コネクションを切断する
+		Connector.getInstance().close();
 
 		//cart.jspに移動
 		resc.setTarget("cart");
