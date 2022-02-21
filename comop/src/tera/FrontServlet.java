68996d8265d577de7ddb19b2b5e2dd53db2e40a0
wcyt <yw20304042@ga.tera-house.ac.jp>
2022-02-21 12:08:44 +0900

    Mavenに変更

--- comop/src/tera/FrontServlet.java
+++ comop/src/tera/FrontServlet.java
@@ -3,7 +3,6 @@ package tera;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;;
@@ -11,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;;
 /**
  * Servlet implementation class FrontServlet
  */
-@WebServlet("/FrontServlet")
+//@WebServlet("/FrontServlet")
 public class FrontServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
